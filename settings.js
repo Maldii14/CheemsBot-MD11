3bd42b46debd16a0dc6e0bd451bcbf92e9290be6
Maldii mak <164465294+Maldii14@users.noreply.github.com>
2024-05-24 12:49:29 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@
 //re-upload? recode? copy code? give credit ya :)
 //YouTube: @DGXeon
 //Instagram: unicorn_xeon13
-//Telegram: t.me/xeonbotinc
-//GitHub: @DGXeon
-//WhatsApp: +916909137213
+//Telegram: t.me/vintavivian 
+//GitHub: @maldii14
+//WhatsApp: +254104567966
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
 
 const fs = require('fs')
@@ -14,34 +14,34 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "YT: venon" //ur yt chanel name
+global.socialm = "GitHub: maldii14" //ur github or insta name
+global.location = "Kenya, Nairobi, Eldoret " //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = 'x Bot ' //ur bot name
+global.ownernumber = '254104567966' //ur owner number
+global.ownername = 'vivian' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.themeemoji = '🎁'
+global.wm = "x Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "x bot\n\n+254104567966"
+global.creator = "254104567966@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
-global.hituet = 0
+global.premium = ["254104567966"] // Premium User
+global.hituet = 33
 
 //bot sett
 global.typemenu = 'v8' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
-global.autoblocknumber = '92' //set autoblock country code
+global.autoblocknumber = '254' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
-global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
+global.anticall = true //bot blocks user when called
+global.autoswview = true //auto status/story view
 global.adminevent = false //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
@@ -49,8 +49,8 @@ global.mess = {
 	limit: 'Your limit is up!',
 	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
     done: 'Done✓',
-    error: 'Error!',
-    success: 'Here you go!'
+    error: 'Error🚫',
+    success: 'Here you go⚡!'
 }
 //thumbnail
 global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
