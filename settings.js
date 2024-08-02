f8f9d4338875edeeb0da7d2c232cc0c2432a47a9
Maldii mak <164465294+Maldii14@users.noreply.github.com>
2024-08-02 13:12:33 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,34 +14,34 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "YT: Maldives mak" //ur yt chanel name
+global.socialm = "GitHub: Maldii14" //ur github or insta name
+global.location = "Kenya, Nairobi, Nairobi" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = 'KING M v1' //ur bot name
+global.ownernumber = '254758732775' //ur owner number
+global.ownername = 'Maldives mak' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
+global.wagc = "https://whatsapp.com/channel/0029VaWrCuH35fLuVP2iCc2R"
+global.themeemoji = '🌿'
+global.wm = "M Bot Inc."
+global.botscript = 'https://github.com/Maldii14/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.author = " MKM\n\n+254758732775"
+global.creator = "254758732775@s.whatsapp.net"
+global.xprefix = '*'
+global.premium = ["254758732775"] // Premium User
 global.hituet = 0
 
 //bot sett
-global.typemenu = 'v8' // menu type 'v1' => 'v8'
+global.typemenu = 'v4' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
-global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
+global.anticall = true //bot blocks user when called
+global.autoswview = true //auto status/story view
 global.adminevent = false //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
